12e84294186f8b33abe649a79f23c32543ac44e0
Baykov Aleksandr <baykov-1988@mail.ru>
2025-08-30 16:53:10 +0300

    refactored footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,89 +1,36 @@
-//import React from 'react'
-
-import styled from "styled-components";
 import Icon from "../../components/icon/Icon";
 import { FlexWrapper } from "../../components/FlexWrapper";
-import { theme } from '../../styles/Theme';
-import { font } from '../../styles/Common';
+import { S } from "../footer/Footer_Styles";
 
-export default function Footer() {
+const socialItemsData = [
+  { iconId: "telegram" },
+  { iconId: "vk" },
+  { iconId: "linkedin" },
+];
+
+export const Footer: React.FC = () => {
   return (
-    <StyledFooter>
+    <S.Footer>
       <FlexWrapper direction="column" align="center">
-        <Name>Aleksandr</Name>
-        <SocialList>
-          <SocialItem>
-            <SocialLink>
-              <Icon
-                height="21px"
-                width="21px"
-                viewBox="0 0 21px 21px"
-                iconId={"telegram"}
-              ></Icon>
-            </SocialLink>
-          </SocialItem>
-          <SocialItem>
-            <SocialLink>
-              <Icon
-                height="21px"
-                width="21px"
-                viewBox="0 0 21px 21px"
-                iconId={"vk"}
-              ></Icon>
-            </SocialLink>
-          </SocialItem>
-          <SocialItem>
-            <SocialLink>
-              <Icon
-                height="21px"
-                width="21px"
-                viewBox="0 0 21px 21px"
-                iconId={"linkedin"}
-              ></Icon>
-            </SocialLink>
-          </SocialItem>
-        </SocialList>
-        <Copyright>@ 2025 Aleksandr Baykov, All Right Reserved</Copyright>
+        <S.Name>Aleksandr</S.Name>
+        <S.SocialList>
+          {socialItemsData.map((item, index) => {
+            return (
+              <S.SocialItem key={index}>
+                <S.SocialLink>
+                  <Icon
+                    height="21px"
+                    width="21px"
+                    viewBox="0 0 21px 21px"
+                    iconId={item.iconId}
+                  ></Icon>
+                </S.SocialLink>
+              </S.SocialItem>
+            );
+          })}
+        </S.SocialList>
+        <S.Copyright>@ 2025 Aleksandr Baykov, All Right Reserved</S.Copyright>
       </FlexWrapper>
-    </StyledFooter>
+    </S.Footer>
   );
-}
-
-const StyledFooter = styled.footer`
-  background-color: ${theme.colors.primaryBg};
-  padding: 40px 0;
-`;
-
-const Name = styled.span`
-  ${font({ family: "'Josefin Sans', sans-serif", weight: 700, Fmax: 22, Fmin: 16})}
-  letter-spacing: 3px;
-`;
-
-const SocialList = styled.ul`
-  display: flex;
-  gap: 20px;
-  margin: 30px 0
-`;
-
-const SocialItem = styled.li``;
-
-const SocialLink = styled.a`
-background-color: rgba(255, 255, 255, 0.1);
-border-radius: 50%;
-width: 35px;
-height: 35px;
-display: flex;
-justify-content: center;
-align-items: center;
-color: ${theme.colors.accent};
-
-&:hover {
-  color: ${theme.colors.primaryBg};
-  transform: translateY(-4px);
-}`;
-
-const Copyright = styled.small`
-font-weight: 400;
-font-size: 12px;
-text-align: center;
-opacity: 0.5; `;
+};
--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -1,12 +1,10 @@
-//import React from 'react'
-
 import styled from 'styled-components'
 import { SectionTitle } from '../../../components/SectionTitle'
 import { Button } from '../../../components/Button'
 import { Container } from '../../../components/Container';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 
-export default function Slogan() {
+export const Slogan: React.FC = () => {
   return (
     <StyledSlogan>
       <Container>
