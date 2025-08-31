f7249bf279c5c7f607b774e6f33f0b2a72d68265
Baykov Aleksandr <baykov-1988@mail.ru>
2025-08-31 12:05:04 +0300

    added useState for mobileMenu

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,19 +1,20 @@
-
 import { Menu } from "../menu/Menu";
 import { S } from "../HeaderMenu_Styles";
+import { useState } from 'react';
 
 export const MobileMenu: React.FC<{ menuItems: Array<string> }> = (props: {
   menuItems: Array<string>;
 }) => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const onBurgerBtnClick = () => {setMenuIsOpen(!menuIsOpen)}
   return (
     <S.MobileMenu>
-      <S.BurgerButton isOpen={false}>
+      <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerBtnClick}>
         <span></span>
       </S.BurgerButton>
-      <S.MobileMenuPopap isOpen={false}>
+      <S.MobileMenuPopap isOpen={menuIsOpen} onClick={() => setMenuIsOpen(false)}>
         <Menu menuItems={props.menuItems} />
       </S.MobileMenuPopap>
     </S.MobileMenu>
   );
 };
-
