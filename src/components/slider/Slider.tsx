ade798d8956f4bc2c081abfae56adfdcc845ec43
Baykov Aleksandr <baykov-1988@mail.ru>
2025-09-01 21:29:36 +0300

    added lib for testimony section

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,24 +1,66 @@
-import { FlexWrapper } from "../FlexWrapper";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 import { S } from "./Slider_Styles";
+import './../../styles/slider.css'
 
-export const Slider: React.FC = () => {
+type SlidePropTypes = {
+  text: string;
+  userName: string
+}
+
+const Slide = (props: SlidePropTypes) => {
   return (
-    <S.Slider>
-      <FlexWrapper>
-        <S.Slide>
-          <S.Text>
-            Excellent, well done, good job, Excellent, well done, good job ,
-            Excellent, well done, good job ,fantastic, Excellent, well done,
-            good job , Excellent, well done, good job
-          </S.Text>
-          <S.Name>@IvanIvanov</S.Name>
-        </S.Slide>
-      </FlexWrapper>
-      <S.Pagination>
-        <span></span>
-        <span className="active"></span>
-        <span></span>
-      </S.Pagination>
-    </S.Slider>
+    <S.Slide>
+      <S.Text>{props.text}
+      </S.Text>
+      <S.Name>@{props.userName}</S.Name>
+    </S.Slide>
   );
 };
+
+const items = [
+  <Slide
+    text="Excellent, well done, good job, Excellent, well done, good job "
+    userName="Ivan Ivanov"
+  />,
+  <Slide
+    text="Excellent, well done, good job, Excellent, well done, good job "
+    userName="Natasha Baykova"
+  />,
+  <Slide
+    text="Excellent, well done, good job, Excellent, well done, good job "
+    userName="Igor Zhukov"
+  />,
+  <Slide
+    text="Excellent, well done, good job, Excellent, well done, good job "
+    userName="Den gerasimov"
+  />
+];
+
+export const Slider = () => (
+  <S.Slider>
+    <AliceCarousel mouseTracking items={items} />
+  </S.Slider>
+);
+
+// export const Slider: React.FC = () => {
+//   return (
+//     <S.Slider>
+//       <FlexWrapper>
+//         <S.Slide>
+//           <S.Text>
+//             Excellent, well done, good job, Excellent, well done, good job ,
+//             Excellent, well done, good job ,fantastic, Excellent, well done,
+//             good job , Excellent, well done, good job
+//           </S.Text>
+//           <S.Name>@IvanIvanov</S.Name>
+//         </S.Slide>
+//       </FlexWrapper>
+//       <S.Pagination>
+//         <span></span>
+//         <span className="active"></span>
+//         <span></span>
+//       </S.Pagination>
+//     </S.Slider>
+//   );
+// };
--- src/components/slider/Slider_Styles.ts
+++ src/components/slider/Slider_Styles.ts
@@ -3,6 +3,7 @@ import { theme } from "../../styles/Theme";
 
 const Slider = styled.div`
   max-width: 500px;
+  width: 100%;
 `;
 
 const Slide = styled.div`
