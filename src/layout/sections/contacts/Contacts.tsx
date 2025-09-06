917178cd45ebf3e3cf5879839e816dd22a918fbc
Baykov Aleksandr <baykov-1988@mail.ru>
2025-09-06 11:19:44 +0300

    added Emailjs on contact section

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -2,16 +2,42 @@ import { SectionTitle } from "../../../components/SectionTitle";
 import { Button } from "../../../components/Button";
 import { Container } from "../../../components/Container";
 import { S } from "../contacts/Contacts_Styles";
+import emailjs from "@emailjs/browser";
+import { ElementRef, useRef } from "react";
 
 export const Contact: React.FC = () => {
+  const form = useRef<ElementRef<"form">>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!form.current) {
+      return;
+    }
+
+    emailjs
+      .sendForm("service_arv2mhy", "template_lsqr8fg", form.current, {
+        publicKey: "yMPQ5mSa-h0st5Vs2",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+    e.currentTarget.reset();
+  };
   return (
     <S.Contacts id={"contact"}>
       <Container>
         <SectionTitle>Contact</SectionTitle>
-        <S.Form>
-          <S.Field placeholder="name" />
-          <S.Field placeholder="subject" />
-          <S.Field placeholder="message" as={"textarea"} />
+        <S.Form ref={form} onSubmit={sendEmail}>
+          <S.Field required placeholder="name" name="name" />
+          <S.Field required placeholder="email" name="email" />
+          <S.Field required placeholder="subject" name="subject" />
+          <S.Field required placeholder="message" as={"textarea"} name="message" />
           <Button type="submit">SEND MESSAGE</Button>
         </S.Form>
       </Container>
