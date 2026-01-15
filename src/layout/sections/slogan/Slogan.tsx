639722e5da83b7d7a8351aa93f3b00026857e714
Baykov Aleksandr <baykov-1988@mail.ru>
2026-01-15 23:30:05 +0300

    add cv to Slogan section

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -1,21 +1,24 @@
 import styled from 'styled-components'
-import { SectionTitle } from '../../../components/SectionTitle'
-import { Button } from '../../../components/Button'
-import { Container } from '../../../components/Container';
-import { FlexWrapper } from '../../../components/FlexWrapper';
+import {SectionTitle} from '../../../components/SectionTitle'
+import {Button} from '../../../components/Button'
+import {Container} from '../../../components/Container';
+import {FlexWrapper} from '../../../components/FlexWrapper';
+import cv from '../../../assets/images/cv.pdf'
+import {Link} from "../../../components/Link.ts";
+
 
 export const Slogan: React.FC = () => {
-  return (
-    <StyledSlogan id={"slogan"}>
-      <Container>
-        <FlexWrapper direction="column" align="center">
-          <SectionTitle>I am available for freelance</SectionTitle>
-          <Button>Hire me</Button>
-        </FlexWrapper>
-      </Container>
-    </StyledSlogan>
-  );
+    return (
+        <StyledSlogan id={"slogan"}>
+            <Container>
+                <FlexWrapper direction="column" align="center">
+                    <SectionTitle>I am available for freelance</SectionTitle>
+                    <Button><Link href={cv} target={"_blank"}>Hire me</Link>
+                    </Button>
+                </FlexWrapper>
+            </Container>
+        </StyledSlogan>
+    );
 }
-
 const StyledSlogan = styled.section`
 `
